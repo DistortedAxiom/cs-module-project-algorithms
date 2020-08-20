514192f1c9f24a9e9b064cb7d27955b540ef6640
Disto <48456332+DistortedAxiom@users.noreply.github.com>
2020-08-20 17:47:28 -0400

    Optimized cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,7 +2,7 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+def eating_cookies(n, cache = None):
 
     if (n < 0):
         return 0
@@ -10,8 +10,18 @@ def eating_cookies(n):
     if (n < 2):
         return 1
 
+    elif cache is not None and cache[n] > 0:
+
+        return cache[n]
+
     else:
-        return eating_cookies((n-1)) + eating_cookies((n-2)) + eating_cookies((n-3))
+
+        if cache is None:
+            cache = [0 for i in range(n + 1)]
+
+        cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+
+    return cache[n]
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
